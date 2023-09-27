af5aad58fb1004f6264688016ee41e83a3f06431
Nalin Kumar <nalin12894+github@gmail.com>
2023-09-27 18:35:14 +0000

    memory allocation of data type

--- 01-basics/data_type_summery.js
+++ 01-basics/data_type_summery.js
@@ -14,7 +14,31 @@ const myFun = function(){
 }
 
 
-console.log(typeof myFun)
+// console.log(typeof myFun)
 
 
 // https://262.ecma-international.org/5.1/#sec-11.4.3
+
+
+//++++++++++++++++++++Memory Allocation in Js+++++++++++++++++++++++
+
+// stack (Primitive) i.e call by value, Heap (Non_primitive) i.e call by reference
+
+
+let str1 = "abcd";
+
+let str2 = str1;
+
+str2= "xyz";
+
+// console.log(str1);
+// console.log(str2);
+
+
+let obj1={name:"nk", email:"nk@test.com"};
+let obj2= obj1;
+obj2.name="test";
+
+console.log(obj1.name);
+console.log(obj2.name);
+
