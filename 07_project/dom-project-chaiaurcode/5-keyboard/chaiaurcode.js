c78eca1a052f2093d093b240e0793713cd4da679
Nalin Kumar <nalin12894+github@gmail.com>
2023-10-02 19:22:18 +0000

    keydown event project

--- 07_project/dom-project-chaiaurcode/5-keyboard/chaiaurcode.js
+++ 07_project/dom-project-chaiaurcode/5-keyboard/chaiaurcode.js
@@ -1,1 +1,24 @@
-console.log('Project 5');
+const key = document.querySelector(".key");
+
+// document.addEventListener('keydown', (e)=>{
+//     key.innerHTML=e.key;
+// })
+window.addEventListener('keydown', (e) => {
+    key.innerHTML = `
+      <div class='color'>
+      <table>
+      <tr>
+        <th>Key</th>
+        <th>Keycode</th> 
+        <th>Code</th>
+      </tr>
+      <tr>
+        <td>${e.key === ' ' ? 'Space' : e.key}</td>
+        <td>${e.keyCode}</td> 
+        <td>${e.code}</td>
+      </tr>
+      
+    </table>
+      </div>
+    `;
+  });
