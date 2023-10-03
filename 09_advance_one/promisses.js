6ee84b84bbea262cdf65bb6423299bf8cc420f71
Nalin Kumar <nalin12894+github@gmail.com>
2023-10-03 18:49:05 +0000

    promose with async and await

--- 09_advance_one/promisses.js
+++ 09_advance_one/promisses.js
@@ -32,3 +32,81 @@ const promisThree = new Promise((resolve, reject) => {
 promisThree.then((user) => {
   console.log("promise consume", user);
 });
+
+const promisFour = new Promise((resolve, reject) => {
+  //Do an async task
+  //DB call, networkCall
+  setTimeout(() => {
+    console.log("_________________________________________________");
+    let error = true;
+    if (!error) {
+      resolve({ username: "NK", email: "test@test.com" });
+    } else {
+      reject("ERROR : somthing went rong");
+    }
+  }, 1000);
+});
+
+promisFour
+  .then((user) => {
+    console.log("promise consume", user);
+    return user.username;
+  })
+  .then((userName) => {
+    console.log("promise consume userName", userName);
+  })
+  .catch((error) => {
+    console.log(error);
+  })
+  .finally(() =>
+    console.log("Finaly : the promise is either resolved or rejected")
+  );
+
+const promisFive = new Promise((resolve, reject) => {
+  //Do an async task
+  //DB call, networkCall
+  setTimeout(() => {
+    console.log("_________________________________________________");
+    let error = true;
+    if (!error) {
+      resolve({ username: "NK", password: "wdr3123" });
+    } else {
+      reject("ERROR : JS went rong");
+    }
+  }, 1000);
+});
+
+async function consumePromiseFive() {
+  try {
+    const response = await promisFive;
+    console.log(response);
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+consumePromiseFive();
+
+// async function getAllUsers() {
+//   try {
+//     const response = await fetch("https://jsonplaceholder.typicode.com/users");
+//     console.log(response);
+//     const data = await response.json(); // used await
+//     console.log(data);
+//   } catch (err) {
+//     console.log(err);
+//   }
+// }
+
+// getAllUsers();
+
+fetch("https://api.github.com/users/nikhil12894")
+  .then((response) => {
+    return response.json();
+  })
+  .then((data) => {
+    console.log(data);
+  })
+  .catch((error) => {
+    console.log(error);
+  });
